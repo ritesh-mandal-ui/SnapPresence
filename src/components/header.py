2d68f198e2eca2707e84fc86c01343051b719a6a
Ritesh Mandal <riteshkumar.mandal0704@gmail.com>
2026-09-19 16:49:29 +0530

    Fix home page logo alignment

--- src/components/header.py
+++ src/components/header.py
@@ -17,15 +17,33 @@ def header_home():
 
     logo_path = get_logo_path()
 
-    col1, col2, col3 = st.columns(
-        [1, 2, 1]
-    )
+    with open(logo_path, "rb") as file:
+        import base64
 
-    with col2:
-        st.image(
-            logo_path,
-            width=300
-        )
+        logo_base64 = base64.b64encode(file.read()).decode()
+
+    st.markdown(
+        f"""
+        <div style="
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0 auto 20px auto;
+        ">
+            <img
+                src="data:image/png;base64,{logo_base64}"
+                style="
+                    width: 300px;
+                    max-width: 100%;
+                    display: block;
+                    margin: 0 auto;
+                "
+            >
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 
 def header_dashboard():
