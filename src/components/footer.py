eed8d399f224cf40676b60ad746ce6044de569b4
Ritesh Mandal <riteshkumar.mandal0704@gmail.com>
2026-09-19 18:10:43 +0530

    Fix footer alignment

--- src/components/footer.py
+++ src/components/footer.py
@@ -46,7 +46,40 @@ def footer_home():
         }
 
         .st-key-home-footer [data-testid="stHorizontalBlock"] {
-            gap: 0.25rem !important;
+            gap: 0 !important;
+            justify-content: center !important;
+        }
+
+        .st-key-home-footer [data-testid="column"] {
+            padding: 0 !important;
+        }
+
+        .st-key-home-footer [data-testid="stImage"] {
+            display: flex !important;
+            justify-content: flex-start !important;
+            align-items: center !important;
+            margin: 0 !important;
+        }
+
+        .st-key-home-footer [data-testid="stMarkdownContainer"] {
+            display: flex !important;
+            justify-content: flex-end !important;
+            align-items: center !important;
+            height: 100% !important;
+            white-space: nowrap !important;
+        }
+
+        @media (max-width: 768px) {
+
+            .st-key-home-footer {
+                padding: 5px 8px 7px 8px !important;
+            }
+
+            .st-key-home-footer [data-testid="stHorizontalBlock"] {
+                width: fit-content !important;
+                margin: 0 auto !important;
+            }
+
         }
 
         </style>
@@ -57,14 +90,14 @@ def footer_home():
     with st.container(key="home-footer"):
 
         col1, col2, col3 = st.columns(
-            [1, 1.2, 1],
+            [1, 1.8, 1],
             vertical_alignment="center"
         )
 
         with col2:
 
             footer_col1, footer_col2 = st.columns(
-                [1.15, 0.85],
+                [1.2, 1],
                 gap="small",
                 vertical_alignment="center"
             )
@@ -102,13 +135,17 @@ def footer_dashboard():
             border-radius: 0 !important;
         }
 
+        .st-key-home-footer [data-testid="stHorizontalBlock"] {
+            gap: 0 !important;
+        }
+
         </style>
         """,
         unsafe_allow_html=True
     )
 
     col1, col2 = st.columns(
-        [1.5, 1],
+        [1, 1],
         vertical_alignment="center"
     )
 
